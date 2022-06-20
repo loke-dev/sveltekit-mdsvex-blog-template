a5e733fb0640214aec5dbf7c8cd9d420dbb42c90
Loke Carlsson <hi@loke.dev>
2022-06-20 09:51:46 +0000

    feat: intial setup

---
+++ src/routes/services.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+    import { dev } from "$app/env"
+  
+    export const router = true
+    export const prerender = true
+  </script>
+  
+  <svelte:head>
+    <title>Services - loke.dev</title>
+    <meta name="description" content="Some of the web development services I can provide help with." />
+  </svelte:head>
+  
+  <div class="content">
+    <h1 class="mb-6">How can I help you?</h1>
+  </div>
+  
