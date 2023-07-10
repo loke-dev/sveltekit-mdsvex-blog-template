4d43b2d47ce2ccbce063f537dd7243b6aa158189
Loke <loke.carlsson@fyndiq.com>
2023-07-10 11:59:23 +0200

    chore: update test

--- tests/e2e/homePage.test.ts
+++ tests/e2e/homePage.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from "@playwright/test"
 
 test("index page has expected h1", async ({ page }) => {
   await page.goto("/")
-  expect(await page.textContent("h1")).toBe("Loke —— Dev")
+  expect(await page.textContent("h2")).toBe("Welcome to")
 })
