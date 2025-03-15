f33ae2d5074388481d054fde1afeda27f179d4f9
Loke <lokeinbox@gmail.com>
2025-03-15 21:35:58 +0100

    chore: remove unused Tailwind CSS PostCSS plugin from configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 export default {
   plugins: {
     "postcss-import": {},
-    "@tailwindcss/postcss": {},
     autoprefixer: {},
     ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
   },
